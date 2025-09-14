1aff2bac853dc6a7bd454066a1191d0811812ad4
Ruth17M <manriquezruth2004@gmail.com>
2025-09-14 16:04:49 -0600

    datos fake

--- app/src/main/java/com/ruthm/rmanriquezfoodapp/MainActivity.kt
+++ app/src/main/java/com/ruthm/rmanriquezfoodapp/MainActivity.kt
@@ -3,45 +3,24 @@ package com.ruthm.rmanriquezfoodapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.ruthm.rmanriquezfoodapp.Models.*
+import com.ruthm.rmanriquezfoodapp.Screens.FoodApp
 import com.ruthm.rmanriquezfoodapp.ui.theme.RManriquezFoodAppTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            RManriquezFoodAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            FoodApp()
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RManriquezFoodAppTheme {
-        Greeting("Android")
-    }
-}
+
+
+
